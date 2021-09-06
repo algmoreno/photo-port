327273543f2eeb27505c6024ab078ede93507b5e
algmoreno <alg.moreno00@gmail.com>
2021-09-06 15:49:28 -0700

    nav test fails

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import coverImage from '../../assets/cover/cover-image.jpg'
+import React from 'react'
+import coverImage from "../../assets/cover/cover-image.jpg";
 
 function About() {
+
   return (
     <section>
-      <h1 id='about'>Who am I?</h1>
+      <h1 id="about">Who am I?</h1>
       <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
     </section>
-  );
+  )
 }
 
-export default About;
+export default About
